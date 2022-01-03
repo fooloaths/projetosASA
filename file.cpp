8544ec60359d6df8e39b6e388d5dc785419ec8db
Gonçalo Nunes <73646319+Goncalo-Palma-Nunes@users.noreply.github.com>
2022-01-03 09:40:15 -0500

    Final version

--- file.cpp
+++ file.cpp
@@ -7,14 +7,11 @@
 #define V1 1
 #define V2 2
 
-/*void printPair(std::vector<int> t) {
-  if (t.size() == 2) {
-    printf("[%d, %d]", t[0], t[1]);
-  }
-  else {
-    printf("Estavamos à espera de um par, mas recebemos algo diferente\n");
-  }
-}*/
+
+void populatePair(long long int value, long long int frequency, std::vector<long long int> &v) {
+  v.push_back(value);
+  v.push_back(frequency);
+}
 
 void stringProcessing(std::vector<int> &v, std::unordered_map<int, int> &map, int context) {
   int c, num, digit, isNegative, isNumber;
@@ -56,45 +53,35 @@ void stringProcessing(std::vector<int> &v, std::unordered_map<int, int> &map, in
 
 }
 
-void populatePair(int value, int frequency, std::vector<int> &v) {
-  v.push_back(value);
-  v.push_back(frequency);
-}
+void insert(std::vector<std::vector<long long int>> &arr, std::vector<long long int> pair) {
+  long long int cmp = pair[0];
 
-void insert(std::vector<std::vector<int>> &arr, std::vector<int> pair) {
-  int cmp = pair[0];
-
-  //printf("O par é "); printPair(pair); printf("\n");
   if (arr[arr.size() - 1][0] > cmp) { //If it is smaller than the smallest tuple on this level
     arr.push_back(pair);
   }
-  for (long unsigned int i = 0; i < arr.size(); i++) {
+  for (long unsigned int i = arr.size() - 1; i >= 0; i--) {
     if (arr[i][0] == cmp) {
       long unsigned int j = i;
       if (((i + 1) < arr.size()) && (arr[i + 1][0] == cmp)) {
         j++;
       }
-      //printf("Estamos no par nº %ld\n", i);
-      //printf("aaaaaaaaaaa\nVamos meter no par "); printPair(arr[i]); printf(" o par "); printPair(pair);
       arr[j] = pair;
       break;
     }
-    else if (arr[i][0] < cmp) {
+    else if (arr[i][0] > cmp) {
       arr.insert(arr.begin() + i, pair);
       break;
     } 
   }
 }
 
-int count(std::vector<std::vector<int>> &pares, int k) {
-    int sizeLIS = pares.size();
-    int counter = 0;
-    int j = sizeLIS - 1;
+int count(std::vector<std::vector<long long int>> &pares, long long int k) {
+    long unsigned int sizeLIS = pares.size();
+    long long int counter = 0;
+    long long int j = sizeLIS - 1;
 
     while ((j >= 0) && (pares[j][0]) < k) {
-      //if (pares[j][0] < k) { //Conditional is a hack to prevent problems with insert at line 86
         counter += pares[j][1];
-      //}
       j--;
     }
     if (j < 0) {j++;}
@@ -105,10 +92,10 @@ int count(std::vector<std::vector<int>> &pares, int k) {
     return counter;
 }
 
-void processValue(std::vector<std::vector<std::vector<int>>> &arr, int k) {
-  int size = arr.size();
+void processValue(std::vector<std::vector<std::vector<long long int>>> &arr, long long int k) {
+  long unsigned int size = arr.size();
 
-  for (int i = size - 1; i >= 0; i--) {
+  for (long unsigned int i = size - 1; i >= 0; i--) {
     auto pares = arr[i];
     
     if (k < pares[pares.size() - 1][0]) {
@@ -119,7 +106,7 @@ void processValue(std::vector<std::vector<std::vector<int>>> &arr, int k) {
     if (counter == DUPLICATE) {
       if (i > 0) { //If it isn't the lowest level
         counter = count(arr[i - 1], k);
-        std::vector<int> pair = std::vector<int>();
+        std::vector<long long int> pair = std::vector<long long int>();
         populatePair(k, counter, pair);
         insert(arr[i], pair);
       }
@@ -127,14 +114,14 @@ void processValue(std::vector<std::vector<std::vector<int>>> &arr, int k) {
     }
     if (counter > 0) {
       if (i < (size - 1)) { //Is within bounds
-        std::vector<int> pair = std::vector<int>();
+        std::vector<long long int> pair = std::vector<long long int>();
         populatePair(k, counter, pair);
         insert(arr[i + 1], pair);
         break;
       }
       else {
-        std::vector<std::vector<int>> tmp = std::vector<std::vector<int>>();
-        std::vector<int> pair = std::vector<int>();
+        std::vector<std::vector<long long int>> tmp = std::vector<std::vector<long long int>>();
+        std::vector<long long int> pair = std::vector<long long int>();
         populatePair(k, counter, pair);
         tmp.push_back(pair);
         arr.push_back(tmp);
@@ -144,41 +131,40 @@ void processValue(std::vector<std::vector<std::vector<int>>> &arr, int k) {
   }
 }
 
-std::vector<int> problem1(std::vector<int> nums) {
-  int size = nums.size();
-  auto aux = std::vector<std::vector<std::vector<int>>>();
+std::vector<long long int> problem1(std::vector<int> nums) {
+  long unsigned int size = nums.size();
+  auto aux = std::vector<std::vector<std::vector<long long int>>>();
 
   if (size > 0) {
-    std::vector<int> pair = std::vector<int>();
+    std::vector<long long int> pair = std::vector<long long int>();
     populatePair(nums.at(0), 1, pair);
-    auto tmp = std::vector<std::vector<int>>();
+    auto tmp = std::vector<std::vector<long long int>>();
     tmp.push_back(pair);
     aux.push_back(tmp);
   }
 
-  for (int i = 1; i < size; i++) {
-    int k = nums[i];
-    int sizeFirstLevel = aux[0].size();
+  for (long unsigned int i = 1; i < size; i++) {
+    long long int k = nums[i];
+    long unsigned int sizeFirstLevel = aux[0].size();
 
     if (k < aux[0][sizeFirstLevel - 1][0])  {
       //Smaller than all LIS on this level (smallest pair is always at last index)
-      std::vector<int> p = std::vector<int>();
+      std::vector<long long int> p = std::vector<long long int>();
       populatePair(k, 1, p);
       aux[0].push_back(p);
       continue;
     }
-
     processValue(aux, k);
   }
 
-  int auxLastLevelIndex = aux.size() - 1;
+  long unsigned int auxLastLevelIndex = aux.size() - 1;
   auto auxLastLevel = aux[auxLastLevelIndex];
-  int sum = 0;
+  long long int sum = 0;
   for (long unsigned int i = 0; i <  auxLastLevel.size(); i++) {
     sum += auxLastLevel[i][1];
   }
 
-  std::vector<int> result = std::vector<int>();
+  std::vector<long long int> result = std::vector<long long int>();
   populatePair(auxLastLevelIndex + 1, sum, result);
   return result;
 }
@@ -211,7 +197,7 @@ int main() {
   if (problemType == 1) {
     stringProcessing(v1, map, V1);
     auto result = problem1(v1);
-    printf("%d %d\n", result[0], result[1]);
+    printf("%lld %lld\n", result[0], result[1]);
   }
   else if (problemType == 2) {
     stringProcessing(v1, map, V1);
